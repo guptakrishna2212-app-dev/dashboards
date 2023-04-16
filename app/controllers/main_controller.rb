37f1afe5e286a27432c6f040c3288e1f8f927387
Krishna Gupta <kgupta6@chicagobooth.edu>
2023-04-16 00:16:14 +0000

    currency_converted_first_draft

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -13,4 +13,23 @@ class MainController < ApplicationController
 
     render({:template => "forex.html.erb"})
   end
+
+  def symbolscomplete
+    require "open-uri"
+    require "json"
+
+    a = open("https://api.exchangerate.host/symbols").read
+
+    @b = JSON.parse(a).fetch("symbols").keys
+    @selected_currency = params.fetch(:from_currency)
+    render({:template => "forexsymbolsfinal.html.erb"})
+  end
+
+  def currency_conversion
+    @first_currency = params.fetch("from_currency")
+    @second_currency = params.fetch("to_currency")
+    raw_data= open("https://api.exchangerate.host/convert?from=#{@first_currency}&to=#{@second_currency}").read
+    @parsed_data = JSON.parse(raw_data).fetch("info").fetch("rate")
+    render({:template => "forexconverted.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get("/", {:controller => "main", :action => "main"})
 
   get("/forex", {:controller => "main", :action => "forex"})
+
+  get("/forex/:from_curreny", {:controller => "main", :action => "symbolscomplete"})
+
+  get("/forex/:from_currency/:to_currency", {:controller => "main", :action => "currency_conversion" })
 end
